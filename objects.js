91685255007fb18fdf0497de57c5971ba4fc20c7
Rwanda S <rwanda.ace@gmail.com>
2020-01-23 18:07:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,5 +3,8 @@ var playlist = {artistName: "songTitle"}
 function updatePlaylist(obj, artistName, songTitle){
  Object.assign(obj, {[artistName]:[songTitle]} )
 return obj
-  
+}
+function removeFromPlaylist(obj, artistName){
+  delete obj[artistName]
+  return obj
 }
